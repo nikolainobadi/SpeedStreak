10e3bbe1f4696d86c386e7ec1347e691ed81e04f
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2024-02-24 16:32:39 -0800

    Initial Commit

---
+++ SpeedStreak/SpeedStreakApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeedStreakApp.swift
+//  SpeedStreak
+//
+//  Created by Nikolai Nobadi on 2/24/24.
+//
+
+import SwiftUI
+
+@main
+struct SpeedStreakApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
