df52a9caca92d0a6c632893e204fc153f6a60ab9
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2024-02-24 16:35:54 -0800

    initial commit

---
+++ SpeedStreak/Sources/Launch/LaunchCoordinatorView.swift
@@ -0,0 +1,20 @@
+//
+//  LaunchCoordinatorView.swift
+//  SpeedStreak
+//
+//  Created by Nikolai Nobadi on 2/24/24.
+//
+
+import SwiftUI
+
+struct LaunchCoordinatorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+
+// MARK: - Preview
+#Preview {
+    LaunchCoordinatorView()
+}
