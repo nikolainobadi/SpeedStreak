32d6806a64d5c0fe6b728a850d460dbf8fcd0d71
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2024-03-09 15:20:53 -0800

    added NoCredentialLoginView

---
+++ SpeedStreak/Sources/UI/NoCredentialLoginView.swift
@@ -0,0 +1,20 @@
+//
+//  NoCredentialLoginView.swift
+//  SpeedStreak
+//
+//  Created by Nikolai Nobadi on 3/9/24.
+//
+
+import SwiftUI
+
+struct NoCredentialLoginView: View {
+    var body: some View {
+        Text("NoCredentialLoginView")
+    }
+}
+
+
+// MARK: - Preview
+#Preview {
+    NoCredentialLoginView()
+}
