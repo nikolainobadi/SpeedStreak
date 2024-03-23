37567ad0bf69b564c455734ad8c2b38f06a44f3a
John Le <johndatle@gmail.com>
2024-03-23 12:28:53 -0700

    Implemented Enable Basic Login

--- SpeedStreak/Sources/Launch/LaunchCoordinatorView.swift
+++ SpeedStreak/Sources/Launch/LaunchCoordinatorView.swift
@@ -8,6 +8,33 @@
 import SwiftUI
 
 struct LaunchCoordinatorView: View {
+    
+    @State private var hasLogInCredentials = true
+    @State private var isNavigating = false
+    
+    var body: some View {
+        NavigationStack {
+            Button("LOG IN"){
+                isNavigating = true
+            }
+            .navigationDestination(isPresented: $isNavigating) {
+                if hasLogInCredentials == true {
+                    hasLogInCredentials_TrueView()
+                } else {
+                    hasLogInCredentials_FalseView()
+                }
+            }
+        }
+    }
+}
+
+struct hasLogInCredentials_TrueView: View {
+    var body: some View {
+        TempInAppView()
+    }
+}
+
+struct hasLogInCredentials_FalseView: View {
     var body: some View {
         NoCredentialLoginView()
     }
