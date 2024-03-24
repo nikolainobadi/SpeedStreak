3d28d7950a0165452f93a005f901a19d6515f22d
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2024-03-23 17:38:43 -0700

    added MARKs for next steps

--- SpeedStreak/Sources/Launch/LaunchCoordinatorView.swift
+++ SpeedStreak/Sources/Launch/LaunchCoordinatorView.swift
@@ -8,16 +8,26 @@
 import SwiftUI
 
 struct LaunchCoordinatorView: View {
-    
     @State private var hasLogInCredentials = true
+    
+    // MARK: - NOT NEEDED
     @State private var isNavigating = false
     
     var body: some View {
+        // MARK: - NOT NEEDED
+        // rather than using a navigation stack, you can simply put the code from the navigationDestination here
+        // so replace the NavStack with the if/else that either shows TempInAppView or NoCredentialLoginView pending on hasLogInCredentials
         NavigationStack {
+            
+            // MARK: - RELOCATE
+            // move this button to NoCredentialLoginView
             Button("LOG IN"){
                 isNavigating = true
             }
             .navigationDestination(isPresented: $isNavigating) {
+                // MARK: - TODO
+                // since hasLogInCredentials is a Bool (true/false), it is fine to simply check the condition with:
+                //   if hasLoginCredentials { 
                 if hasLogInCredentials == true {
                     hasLogInCredentials_TrueView()
                 } else {
@@ -28,12 +38,17 @@ struct LaunchCoordinatorView: View {
     }
 }
 
+
+// MARK: - NOT NEEDED
+// use TempInAppView directly, no need to wrap it in another view
 struct hasLogInCredentials_TrueView: View {
     var body: some View {
         TempInAppView()
     }
 }
 
+// MARK: - NOT NEEDED
+// use NoCredentialLoginView directly, no need to wrap it in another view
 struct hasLogInCredentials_FalseView: View {
     var body: some View {
         NoCredentialLoginView()
--- SpeedStreak/Sources/UI/NoCredentialLoginView.swift
+++ SpeedStreak/Sources/UI/NoCredentialLoginView.swift
@@ -8,7 +8,26 @@
 import SwiftUI
 
 struct NoCredentialLoginView: View {
+    // MARK: - TODO
+    // there are 2 ways to enable the login button from here
+    
+    // 1. passing the hasLogInCredentials as @Binding, then toggle in the loginButton action directly
+    //   - while convenient, this creates a tight coupling between what it means to 'log-in' and this view.
+    
+    // 2. passing in a closure to represent the action
+    //   - this is a better solution because it no longer matters to this view what it means to login
+    //
+    //   Example:
+    //     let loginAction: () -> Void
+    //
+    //   Usage:
+    //     Button("Log in", action: loginAction)    *this can also be written like this*  Button(action: { loginAction() }, label: { Text("Log in") })
+    //
+    // I think this was a point of confusion in our very first meeting, so if you have questions let me know.
+    
     var body: some View {
+        // MARK: - TODO
+        // add login button here
         Text("NoCredentialLoginView")
     }
 }
