afc6baec7a6760fe36322fe3a61852b177f48f92
John Le <johndatle@gmail.com>
2024-03-29 22:44:20 -0700

    Attempt to make loginAction modular and in a seperate coordinator. But am struggling to get it to work. Could use some guidance.

---
+++ SpeedStreak/Sources/Coordinators/LoginCoordinator.swift
@@ -0,0 +1,18 @@
+//
+//  LoginCoordinator.swift
+//  SpeedStreak
+//
+//  Created by John on 3/29/24.
+//
+
+import SwiftUI
+
+struct LoginCoordinator: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginCoordinator()
+}
