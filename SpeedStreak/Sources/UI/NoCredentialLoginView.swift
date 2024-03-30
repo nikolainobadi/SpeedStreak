9f99ffe00d0210030b573509c2929478534eb417
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2024-03-30 16:37:02 -0700

    added comments to try and explain 'callback' syntax in NoCredentialLoginView

--- SpeedStreak/Sources/UI/NoCredentialLoginView.swift
+++ SpeedStreak/Sources/UI/NoCredentialLoginView.swift
@@ -23,3 +23,39 @@ struct NoCredentialLoginView: View {
 #Preview {
     NoCredentialLoginView(loginAction: { })
 }
+
+
+// MARK: - 'Callback' Syntax
+/// When you write a function, there is always a return type. If you don't define that type, it is Void
+/// so technically something like this
+///
+/// func doSomething() {
+///}
+///
+/// can be translated to this 'behind the scenes
+///
+/// func doSomething() -> Void {
+/// }
+///
+/// Likewise, if you want the function to return an Int or a Bool, you define that in the 'function signature'
+///
+/// func giveMeAnInt() -> Int {
+///}
+///
+/// func giveMeABool() -> Bool {
+/// }
+///
+///
+/// So to pass any of this as an argument, they would appear like this in the struct
+///
+/// let doSomething: () -> Void
+/// let giveMeAnInt: () -> Void
+/// let giveMeAnBool: () -> Void
+///
+/// then these 'callbacks' can be used by invoking the name you used followed by parenthesis
+///
+///
+/// doSomething()
+/// let myInt = giveMeAnInt()
+/// let myBool = giveMeABool()
+
